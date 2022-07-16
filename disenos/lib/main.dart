fa3cc28fcb107030fe48f3d4003b4c3cb74757f1
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2022-07-16 08:16:04 +0200

    init project "diseños"

---
+++ disenos/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      );
+  }
+}
+
+
---
+++ disenos/lib/screens/basic_design.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BasicDesignScreen extends StatelessWidget {
+   
+  const BasicDesignScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('BasicDesignScreen'),
+      ),
+    );
+  }
+}
