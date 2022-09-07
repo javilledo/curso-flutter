90c5d3a8bf0163048d6f9260000a2ea26ae6f8c3
Francisco Javier Lledó Fuster <javilledo@gmail.com>
2022-09-07 21:08:44 +0200

    init project

---
+++ qr_reader/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader/pages/home_page.dart';
+import 'package:qr_reader/pages/mapa_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'QR Reader',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => const HomeScreen(),
+        'mapa': (BuildContext context) => const MapaScreen(),
+      },
+    );
+  }
+}
---
+++ qr_reader/lib/pages/mapa_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MapaScreen extends StatelessWidget {
+   
+  const MapaScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('MapaScreen'),
+      ),
+    );
+  }
+}
